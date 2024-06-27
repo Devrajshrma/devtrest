36292fb90cbef6642f0499660090757d3e7d9d1e
Devrajshrma <devrajsharma0804@gmail.com>
2024-06-27 16:38:39 +0530

    ho gya bas

--- script.js
+++ script.js
@@ -12,7 +12,21 @@ var canvas =[
     {img:"https://i.pinimg.com/474x/b8/a8/ba/b8a8ba70cc5bfb9a902b70f4a849f8be.jpg"},
     {img:"https://i.pinimg.com/236x/f8/3b/71/f83b71cd08d7d8669d1501f777afbc3f.jpg"},
     {img:"https://i.pinimg.com/236x/73/f9/26/73f92619104c4dc9d59d7b3f9d467c25.jpg"},
-    {img:"https://i.pinimg.com/474x/3a/ee/71/3aee710fee92de4a0cbfcca1e6dd61e6.jpg"}
+    {img:"https://i.pinimg.com/474x/3a/ee/71/3aee710fee92de4a0cbfcca1e6dd61e6.jpg"},
+    {img:"https://i.pinimg.com/474x/89/ba/e2/89bae2b7aff475562ef7a773a3e9864a.jpg"},
+    {img:"https://i.pinimg.com/236x/60/d1/ad/60d1ad25697ea008c5a058e13c6d1298.jpg"},
+    {img:"https://i.pinimg.com/474x/8c/44/a3/8c44a3eb18e89454f95b430e93812f25.jpg"},
+    {img:"https://i.pinimg.com/474x/6a/96/b8/6a96b837c84ae86d7bb772fe4d59e917.jpg"},
+    {img:"https://i.pinimg.com/236x/8b/ef/7b/8bef7b56ee204993a6340dffa690c43e.jpg"},
+    {img:"https://i.pinimg.com/236x/07/20/63/072063b20b72f3ef58afab77e8edc6a0.jpg"},
+    {img:"https://i.pinimg.com/474x/06/e3/c4/06e3c433d070d812b1e491266ec79f81.jpg"},
+    {img:"https://i.pinimg.com/474x/06/e3/c4/06e3c433d070d812b1e491266ec79f81.jpg"},
+    {img:"https://i.pinimg.com/474x/d6/45/98/d64598368dee826caddfdd8a4adbf57e.jpg"},
+    {img:"https://i.pinimg.com/474x/2a/d8/61/2ad861f285d392162575cdf3ae81e64c.jpg"},
+    {img:"https://i.pinimg.com/474x/2c/04/50/2c045030c8dcd727943a2f636f811775.jpg"},
+    {img:"https://i.pinimg.com/236x/00/3e/b8/003eb857d4561ebfc71b50eb088550cd.jpg"},
+    {img:"https://i.pinimg.com/236x/ba/be/0f/babe0fd4bce629d4321e2859038e7a3f.jpg"},
+    {img:"https://i.pinimg.com/236x/f9/cc/b4/f9ccb4cf0e2ddb90fa70304ed0e4cdeb.jpg"},
   ]
 
 
@@ -23,8 +37,11 @@ var canvas =[
   ]
 
   var popularONpinterest = [
-    {Image : "https://i.pinimg.com/236x/98/e7/b8/98e7b84f9a03a55c8824abc224067f9d.jpg", name : "Character design"},
-    {Image : "https://i.pinimg.com/236x/8b/91/08/8b9108f4078a5253f79a350f5a4cfc78.jpg", name : "Dp for whatsapp"}
+    {Image : "https://i.pinimg.com/236x/24/84/f6/2484f660ba02117b6521f9d8b9617832.jpg", name : "Dp for whatsapp"},
+    {Image : "https://i.pinimg.com/236x/49/46/16/49461699264a7475f5b2ef0bfb3620d8.jpg", name : "Selfie Posses"},
+    {Image : "https://i.pinimg.com/236x/28/8a/6b/288a6b85623c9e719a3c500c26adecee.jpg", name : "New/Old Posses"}, 
+    {Image : "https://i.pinimg.com/236x/ef/1e/8a/ef1e8a4a967bafbb89d7f82d57e6713c.jpg", name : "Shahu Maharaj"}, 
+    {Image : "https://i.pinimg.com/236x/9b/76/68/9b76684f09e47b8ac38139935e510fdd.jpg", name : "Feel The Nature"} 
   ]
 
 function showCanvas(){
@@ -57,7 +74,7 @@ function showIfy(){
   var dev = ""
   ideaForyou.forEach(function(obj){
       dev += `<div
-          class="h-20 w-64 bg-gray-100 hover:bg-gray-200 flex items-center justify-between gap-2 rounded-2xl cursor-pointer">
+          class="h-20 w-[100%] bg-gray-100 hover:bg-gray-200 flex items-center justify-between gap-2 rounded-2xl cursor-pointer mb-2">
           <img src="${obj.Image}" alt=""
             class="h-full w-full object-cover rounded-l-2xl">
           <div class="font-semibold w-full text-start">${obj.name}</div>
@@ -74,11 +91,13 @@ function showPop(){
   var dev = ""
   popularONpinterest.forEach(function(obj){
       dev += `<div
-          class="h-20 w-64 bg-gray-100 hover:bg-gray-200 flex items-center justify-between gap-2 rounded-2xl cursor-pointer">
-          <img src="${obj.Image}" alt=""
-            class="h-full w-full object-cover rounded-l-2xl">
-          <div class="font-semibold w-full text-start">${obj.name}</div>
-        </div>`
+            class="h-20 w-[100%] bg-gray-100 hover:bg-gray-200 flex items-center justify-between rounded-2xl cursor-pointer mb-5">
+            <div class="w-[60%] h-full bg-black rounded-l-2xl">
+              <img src="${obj.Image}" alt=""
+                class="h-full w-full object-cover rounded-l-2xl">
+            </div>
+            <div class="font-semibold w-auto h-full text-center py-2">${obj.name}</div>
+          </div>`
     document.querySelector("#pop").innerHTML= dev
   
   });
@@ -89,7 +108,8 @@ function showPop(){
 
 document.querySelector(".srch").addEventListener("focusin",function(){
   document.querySelector(".srchc").style.display="block"
-  document.querySelector("body").style.position="fixed"
+  document.querySelector(".srchc").style.position="fixed"
+  document.querySelector("body").style.position="absolute"
   document.querySelector(".black").style.display="block"
   document.querySelector(".srch").style.border="2px solid skyblue"
 })
